11205023c769cd32bd5f443b6f0b4f931a2b88e1
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-04-23 14:29:17 -0400

    Initial commit, created server file

---
+++ index.js
@@ -0,0 +1,7 @@
+//import server from './server.js'
+
+const server = require('./server.js');
+
+server.listen(8000, () => {
+    console.log('\n*** Server Running on http://localhost:8000 ***\n');
+});
